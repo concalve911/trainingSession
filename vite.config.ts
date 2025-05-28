4a4143d195a32e9b81d827fb7732993a9fded943
concalve911 <aleksandrgarkusha911@gmail.com>
2025-05-28 06:56:56 +0200

    third

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,15 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import Inspect from "vite-plugin-inspect";
+import OpenIde from "vite-inspector";
 
 export default defineConfig({
-  plugins: [react()],
+  base: "/trainingSession",
+  plugins: [
+    OpenIde({
+      framework: "react",
+    }),
+    react(),
+    Inspect(),
+  ],
 });
